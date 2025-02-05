6adf2668068089dd94dc05f840cb4b5627d4bbc5
Jeffrey P <jeffp.web@gmail.com>
2025-02-04 17:38:54 -0800

    Remove border from Navbar

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -5,7 +5,7 @@ export default function Header({ onTaskCreate, isDemo }) {
   const { logout, isDemo: authDemo } = useAuth();
 
   return (
-    <header className="bg-neutral-50 border-b-2 border-gray-400">
+    <header className="bg-neutral-50 ">
       <div className="container mx-auto px-4 py-8">
         <div className="flex justify-between items-center">
           <h1 className="text-4xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-blue-500 pb-1">
